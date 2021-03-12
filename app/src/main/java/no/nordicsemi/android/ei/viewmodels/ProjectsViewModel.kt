9c0eb600af6f49d7e796192c39c5a04eda07a0f8
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2021-03-12 15:14:08 +0100

    Adds support for pull to refresh projects and fixes multiple calls to loadProjects.

--- app/src/main/java/no/nordicsemi/android/ei/viewmodels/ProjectsViewModel.kt
+++ app/src/main/java/no/nordicsemi/android/ei/viewmodels/ProjectsViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
 import no.nordicsemi.android.ei.model.Project
 import no.nordicsemi.android.ei.repository.ProjectsRepository
@@ -14,21 +13,40 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProjectsViewModel @Inject constructor(
+    private val token: String,
     private val repo: ProjectsRepository
 ) : ViewModel() {
 
-    private val _projects = MutableLiveData<List<Project>>()
+    private val _projects = MutableLiveData(listOf<Project>())
     val projects: LiveData<List<Project>> = _projects
 
-    @ExperimentalCoroutinesApi
-    fun projects(token: String) {
+    private val _pullToRefresh = MutableLiveData(false)
+    val pullToRefresh: LiveData<Boolean> = _pullToRefresh
+
+    init {
+        loadProjects()
+    }
+
+    /**
+     * Loads projects from the Edge Impulse Studio
+     *
+     * @param withPullToRefresh True if pull to refresh was used to load projects
+     */
+    fun loadProjects(withPullToRefresh: Boolean = false) {
+        if (withPullToRefresh) {
+            _pullToRefresh.value = withPullToRefresh
+        }
+
         viewModelScope.launch {
-            val projectResponse = repo.projects(token = token)
-            Log.i("AA", projectResponse.toString())
-            projectResponse.takeIf {
-                it.success
-            }?.apply {
-                _projects.value = this.projects
+            repo.projects(token = token).run {
+                if (withPullToRefresh)
+                    _pullToRefresh.postValue(false)
+                Log.i("AA", toString())
+                this.takeIf {
+                    it.success
+                }?.apply {
+                    _projects.value = projects
+                }
             }
         }
     }
