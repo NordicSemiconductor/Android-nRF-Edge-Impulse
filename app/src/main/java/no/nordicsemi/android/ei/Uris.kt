e08a38bd5017420af8b729ce989257128276c021
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2021-03-17 14:53:00 +0100

    Uris moved to a file

---
+++ app/src/main/java/no/nordicsemi/android/ei/Uris.kt
@@ -0,0 +1,8 @@
+package no.nordicsemi.android.ei
+
+import android.net.Uri
+
+object Uris {
+    val ForgetPassword: Uri = Uri.parse("https://studio.edgeimpulse.com/forgot-password")
+    val SignUp: Uri = Uri.parse("https://studio.edgeimpulse.com/signup")
+}
