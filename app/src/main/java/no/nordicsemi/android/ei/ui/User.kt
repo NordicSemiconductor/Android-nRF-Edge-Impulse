2c84cca8267ffcb21e4aff290753eb233deea332
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2021-03-22 12:56:17 +0100

    UserAppBar improvements

--- app/src/main/java/no/nordicsemi/android/ei/ui/User.kt
+++ app/src/main/java/no/nordicsemi/android/ei/ui/User.kt
@@ -1,23 +1,26 @@
 package no.nordicsemi.android.ei.ui
 
-import androidx.compose.foundation.*
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import dev.chrisbanes.accompanist.coil.CoilImage
 import no.nordicsemi.android.ei.R
@@ -32,78 +35,110 @@ fun User(
     onRefresh: () -> Unit,
     onLogoutClick: () -> Unit
 ) {
-
     val height: Dp = 150.dp
-    Scaffold {
-        Column {
-            UserContent(
-                height = height,
+    Scaffold(
+        topBar = {
+            UserAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.label_welcome))
+                },
                 user = user,
-                onLogoutClick = onLogoutClick
-            )
-            Projects(
-                height = height,
-                projects = user.projects,
-                refreshingState = refreshingState,
-                onRefresh = { onRefresh() }
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(height),
+                onLogoutClick = onLogoutClick,
             )
         }
+    ) {
+        SwipeToRefreshLayout(
+            refreshingState = refreshingState,
+            onRefresh = onRefresh,
+            refreshIndicator = {
+                Surface(elevation = 10.dp, shape = CircleShape) {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .size(36.dp)
+                            .padding(4.dp)
+                    )
+                }
+            },
+            content = {
+                ProjectsList(
+                    modifier = Modifier.fillMaxSize(),
+                    projects = user.projects
+                )
+            })
     }
 }
 
 @Composable
-fun UserContent(
-    modifier: Modifier = Modifier,
-    height: Dp,
+fun UserAppBar(
+    title: @Composable () -> Unit,
     user: User,
-    onLogoutClick: () -> Unit
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = MaterialTheme.colors.primarySurface,
+    contentColor: Color = contentColorFor(backgroundColor),
+    elevation: Dp = AppBarDefaults.TopAppBarElevation,
+    onLogoutClick: () -> Unit = {},
 ) {
     Box(
         modifier = modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(bottom = 16.dp)
-            .background(color = MaterialTheme.colors.primary)
     ) {
-        Row {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(16.dp),
-                    text = stringResource(R.string.label_welcome),
-                    style = MaterialTheme.typography.h5,
-                    color = MaterialTheme.colors.onPrimary
-                )
+        TopAppBar(
+            modifier = Modifier.fillMaxHeight(0.66f),
+            backgroundColor = backgroundColor,
+            contentColor = contentColor,
+            elevation = elevation
+        ) {
+            Spacer(modifier = Modifier.width(12.dp)) // 16.dp - 4.dp
+            Row(
+                Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .padding(vertical = 8.dp),
+                verticalAlignment = Alignment.Top,
+            ) {
+                ProvideTextStyle(value = MaterialTheme.typography.h6) {
+                    CompositionLocalProvider(
+                        LocalContentAlpha provides ContentAlpha.high,
+                        content = title
+                    )
+                }
             }
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    modifier = Modifier
-                        .clickable(
-                            onClickLabel = stringResource(id = R.string.action_logout),
-                            role = Role.Button,
-                            onClick = onLogoutClick
-                        )
-                        .clip(CircleShape)
-                        .padding(16.dp),
-                    text = stringResource(R.string.action_logout),
-                    style = MaterialTheme.typography.h5,
-                    color = MaterialTheme.colors.onPrimary
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Row(
+                    Modifier.fillMaxHeight(),
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.Top,
+                    content = {
+                        var showMenu by remember { mutableStateOf(false) }
+                        IconButton(onClick = { showMenu = true }) {
+                            Icon(Icons.Default.MoreVert, "More actions")
+                        }
+                        DropdownMenu(
+                            expanded = showMenu,
+                            onDismissRequest = { showMenu = false },
+                            offset = DpOffset(x = 0.dp, y = (-48).dp)
+                        ) {
+                            DropdownMenuItem(onClick = onLogoutClick) {
+                                Text(text = stringResource(id = R.string.action_logout))
+                            }
+                        }
+                    }
                 )
             }
         }
         Row(
-            modifier = modifier
-                .offset(y = height / 2)
-                .fillMaxWidth(),
+            modifier = Modifier
+                .padding(start = 16.dp, top = 48.dp, end = 16.dp)
+                .fillMaxHeight(),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Surface(
-                modifier = modifier
-                    .padding(start = 16.dp)
-                    .size(150.dp)
+                modifier = Modifier
+                    .aspectRatio(1.0f)
                     .border(
-                        border = BorderStroke(4.dp, color = Color.White),
+                        border = BorderStroke(3.dp, color = MaterialTheme.colors.onPrimary),
                         shape = CircleShape
                     ),
                 shape = CircleShape,
@@ -112,112 +147,69 @@ fun UserContent(
                 CoilImage(
                     data = user.photo ?: R.drawable.ic_edge_impulse,
                     contentDescription = stringResource(R.string.content_description_user_image),
-                    modifier = modifier.size(50.dp),
                     alignment = Alignment.Center
                 )
             }
-            Spacer(modifier = Modifier.size(16.dp))
+            Spacer(modifier = Modifier.width(8.dp))
             Column(
-                modifier = Modifier.wrapContentHeight(),
+                modifier = Modifier.fillMaxHeight(),
                 verticalArrangement = Arrangement.Center
             ) {
-                Spacer(
+                Column(
                     modifier = Modifier
-                        .size(16.dp)
-                )
+                        .weight(1f)
+                        .padding(bottom = 4.dp)
+                ) {
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(
+                        text = user.name,
+                        color = MaterialTheme.colors.onPrimary,
+                        style = MaterialTheme.typography.h6,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                }
                 Text(
-                    color = MaterialTheme.colors.onPrimary,
-                    text = user.name,
-                    style = MaterialTheme.typography.h6,
-                    maxLines = 1
-                )
-                Spacer(modifier = Modifier.size(8.dp))
-                Text(
-                    color = MaterialTheme.colors.onBackground,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(top = 4.dp),
                     text = user.email,
-                    style = MaterialTheme.typography.subtitle2,
-                    maxLines = 1
-                )
-                Text(
-                    color = MaterialTheme.colors.onSurface,
-                    text = user.username,
-                    style = MaterialTheme.typography.subtitle2,
-                    maxLines = 1
-                )
-                Spacer(
-                    modifier = Modifier
-                        .size(8.dp)
+                    color = MaterialTheme.colors.primary,
+                    style = MaterialTheme.typography.caption,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
                 )
             }
         }
+
     }
 }
 
 @Composable
-fun Projects(
-    height: Dp,
-    modifier: Modifier = Modifier,
-    projects: List<Project>,
-    refreshingState: Boolean,
-    onRefresh: () -> Unit
-) {
-    SwipeToRefreshLayout(
-        refreshingState = refreshingState,
-        onRefresh = onRefresh,
-        refreshIndicator = {
-            Surface(elevation = 10.dp, shape = CircleShape) {
-                CircularProgressIndicator(
-                    modifier = Modifier
-                        .size(36.dp)
-                        .padding(4.dp)
-                )
-            }
-        },
-        content = {
-            ProjectList(
-                height = height,
-                modifier = modifier,
-                projects = projects
-            )
-        })
-}
-
-@Composable
-fun ProjectList(
-    height: Dp,
+fun ProjectsList(
     modifier: Modifier,
     projects: List<Project>
 ) {
-
     val scrollState = rememberLazyListState()
-    Column(modifier = Modifier.fillMaxSize()) {
-        Spacer(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(height = height / 2)
-        )
-        Column {
+    LazyColumn(
+        modifier = modifier,
+        state = scrollState
+    ) {
+        item {
             Text(
                 modifier = Modifier
-                    .padding(start = 16.dp, top = 8.dp, end = 16.dp),
+                    .padding(start = 12.dp)
+                    .padding(vertical = 16.dp),
                 text = stringResource(id = R.string.title_projects),
                 color = MaterialTheme.colors.onSurface,
                 style = MaterialTheme.typography.h6
             )
-            LazyColumn(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight()
-                    .padding(top = 16.dp),
-                state = scrollState
-            ) {
-                itemsIndexed(items = projects) { _, project ->
-                    ProjectRow(
-                        modifier = modifier,
-                        project = project
-                    )
-                }
-            }
+        }
+        items(items = projects) { project ->
+            ProjectRow(
+                modifier = modifier,
+                project = project
+            )
         }
     }
 }
