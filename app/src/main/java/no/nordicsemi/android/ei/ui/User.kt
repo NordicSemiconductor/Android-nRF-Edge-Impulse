5b13570ccbfa532f7f9067dc6efecdc861bfc569
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2021-03-22 14:37:51 +0100

    UserAppBar improvements, part 2

--- app/src/main/java/no/nordicsemi/android/ei/ui/User.kt
+++ app/src/main/java/no/nordicsemi/android/ei/ui/User.kt
@@ -1,32 +1,25 @@
 package no.nordicsemi.android.ei.ui
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.DpOffset
-import androidx.compose.ui.unit.dp
-import dev.chrisbanes.accompanist.coil.CoilImage
+import androidx.compose.ui.unit.*
 import no.nordicsemi.android.ei.R
 import no.nordicsemi.android.ei.model.Project
 import no.nordicsemi.android.ei.model.User
 import no.nordicsemi.android.ei.ui.layouts.SwipeToRefreshLayout
+import no.nordicsemi.android.ei.ui.layouts.UserAppBar
 
 @Composable
 fun User(
@@ -35,7 +28,6 @@ fun User(
     onRefresh: () -> Unit,
     onLogoutClick: () -> Unit
 ) {
-    val height: Dp = 150.dp
     Scaffold(
         topBar = {
             UserAppBar(
@@ -43,9 +35,6 @@ fun User(
                     Text(text = stringResource(id = R.string.label_welcome))
                 },
                 user = user,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(height),
                 onLogoutClick = onLogoutClick,
             )
         }
@@ -72,120 +61,6 @@ fun User(
 }
 
 @Composable
-fun UserAppBar(
-    title: @Composable () -> Unit,
-    user: User,
-    modifier: Modifier = Modifier,
-    backgroundColor: Color = MaterialTheme.colors.primarySurface,
-    contentColor: Color = contentColorFor(backgroundColor),
-    elevation: Dp = AppBarDefaults.TopAppBarElevation,
-    onLogoutClick: () -> Unit = {},
-) {
-    Box(
-        modifier = modifier
-    ) {
-        TopAppBar(
-            modifier = Modifier.fillMaxHeight(0.66f),
-            backgroundColor = backgroundColor,
-            contentColor = contentColor,
-            elevation = elevation
-        ) {
-            Spacer(modifier = Modifier.width(12.dp)) // 16.dp - 4.dp
-            Row(
-                Modifier
-                    .fillMaxHeight()
-                    .weight(1f)
-                    .padding(vertical = 8.dp),
-                verticalAlignment = Alignment.Top,
-            ) {
-                ProvideTextStyle(value = MaterialTheme.typography.h6) {
-                    CompositionLocalProvider(
-                        LocalContentAlpha provides ContentAlpha.high,
-                        content = title
-                    )
-                }
-            }
-            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Row(
-                    Modifier.fillMaxHeight(),
-                    horizontalArrangement = Arrangement.End,
-                    verticalAlignment = Alignment.Top,
-                    content = {
-                        var showMenu by remember { mutableStateOf(false) }
-                        IconButton(onClick = { showMenu = true }) {
-                            Icon(Icons.Default.MoreVert, "More actions")
-                        }
-                        DropdownMenu(
-                            expanded = showMenu,
-                            onDismissRequest = { showMenu = false },
-                            offset = DpOffset(x = 0.dp, y = (-48).dp)
-                        ) {
-                            DropdownMenuItem(onClick = onLogoutClick) {
-                                Text(text = stringResource(id = R.string.action_logout))
-                            }
-                        }
-                    }
-                )
-            }
-        }
-        Row(
-            modifier = Modifier
-                .padding(start = 16.dp, top = 48.dp, end = 16.dp)
-                .fillMaxHeight(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Surface(
-                modifier = Modifier
-                    .aspectRatio(1.0f)
-                    .border(
-                        border = BorderStroke(3.dp, color = MaterialTheme.colors.onPrimary),
-                        shape = CircleShape
-                    ),
-                shape = CircleShape,
-                elevation = 10.dp
-            ) {
-                CoilImage(
-                    data = user.photo ?: R.drawable.ic_edge_impulse,
-                    contentDescription = stringResource(R.string.content_description_user_image),
-                    alignment = Alignment.Center
-                )
-            }
-            Spacer(modifier = Modifier.width(8.dp))
-            Column(
-                modifier = Modifier.fillMaxHeight(),
-                verticalArrangement = Arrangement.Center
-            ) {
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(bottom = 4.dp)
-                ) {
-                    Spacer(modifier = Modifier.weight(1f))
-                    Text(
-                        text = user.name,
-                        color = MaterialTheme.colors.onPrimary,
-                        style = MaterialTheme.typography.h6,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                    )
-                }
-                Text(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(top = 4.dp),
-                    text = user.email,
-                    color = MaterialTheme.colors.primary,
-                    style = MaterialTheme.typography.caption,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                )
-            }
-        }
-
-    }
-}
-
-@Composable
 fun ProjectsList(
     modifier: Modifier,
     projects: List<Project>
@@ -199,7 +74,7 @@ fun ProjectsList(
             Text(
                 modifier = Modifier
                     .padding(start = 12.dp)
-                    .padding(vertical = 16.dp),
+                    .padding(top = 72.dp, bottom = 16.dp),
                 text = stringResource(id = R.string.title_projects),
                 color = MaterialTheme.colors.onSurface,
                 style = MaterialTheme.typography.h6
---
+++ app/src/main/java/no/nordicsemi/android/ei/ui/layouts/UserTopBarShape.kt
@@ -0,0 +1,29 @@
+package no.nordicsemi.android.ei.ui.layouts
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.geometry.toRect
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+
+class UserTopBarShape(
+    private val imageSize: Size,
+    private val offset : Offset
+): Shape {
+
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        val path = Path()
+        path.addOval(imageSize.toRect())
+        path.translate(offset)
+        path.addRect(size.toRect())
+        return Outline.Generic(path)
+    }
+
+}
