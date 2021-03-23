20532039fbee515042e9fe7dda9e3f889b5f2931
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2021-03-23 13:54:59 +0100

    Minor polish

--- app/src/main/java/no/nordicsemi/android/ei/ui/User.kt
+++ app/src/main/java/no/nordicsemi/android/ei/ui/User.kt
@@ -9,12 +9,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -74,7 +71,6 @@ fun ProjectsList(
     projects: List<Project>
 ) {
     val scrollState = rememberLazyListState()
-    val horizontalPadding =
     LazyColumn(
         modifier = modifier,
         contentPadding = PaddingValues(top = 72.dp, bottom = 36.dp),
