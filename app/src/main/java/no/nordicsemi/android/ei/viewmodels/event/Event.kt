ac7cb65c77aa92e2d605ed2b3b9fdac4a25c0351
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2021-04-12 15:18:26 +0200

    Improve events

--- app/src/main/java/no/nordicsemi/android/ei/viewmodels/event/Event.kt
+++ app/src/main/java/no/nordicsemi/android/ei/viewmodels/event/Event.kt
@@ -2,5 +2,6 @@ package no.nordicsemi.android.ei.viewmodels.event
 
 sealed class Event {
     object DismissDialog : Event()
-    data class ShowSnackbar(val message: String) : Event()
+    data class ProjectCreated(val projectName: String) : Event()
+    data class Error(val throwable: Throwable) : Event()
 }
