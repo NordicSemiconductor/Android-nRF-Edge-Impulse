d385be380e1289ae61b0292821c46de66aa20f5b
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2021-04-14 14:19:46 +0200

    WIP: Bottom navigation routes

--- app/src/main/java/no/nordicsemi/android/ei/Route.kt
+++ app/src/main/java/no/nordicsemi/android/ei/Route.kt
@@ -2,6 +2,11 @@ package no.nordicsemi.android.ei
 
 object Route {
     const val splashscreen = "splashscreen"
-    const val user = "user"
-    const val projects = "projects"
+    const val dashboard = "dashboard"
+    const val project = "project"
+
+    //BottomNavRoutes
+    const val devices = "devices"
+    const val dataAcquisition = "dataAcquisition"
+    const val deployment = "deployment"
 }
