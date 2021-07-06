167d852079ecc28fd52d24e4cdb603e96de55170
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2021-07-06 14:11:16 +0200

    Inject socket token in the project component

--- app/src/main/java/no/nordicsemi/android/ei/di/ProjectComponent.kt
+++ app/src/main/java/no/nordicsemi/android/ei/di/ProjectComponent.kt
@@ -4,6 +4,7 @@ import dagger.BindsInstance
 import dagger.hilt.DefineComponent
 import no.nordicsemi.android.ei.model.DevelopmentKeys
 import no.nordicsemi.android.ei.model.Project
+import no.nordicsemi.android.ei.model.SocketToken
 
 @ProjectScope
 @DefineComponent(parent = UserComponent::class)
@@ -13,6 +14,7 @@ interface ProjectComponent {
     interface Builder {
         fun setProject(@BindsInstance project: Project): Builder
         fun setKeys(@BindsInstance keys: DevelopmentKeys): Builder
+        fun setSocketToken(@BindsInstance keys: SocketToken): Builder
         fun build(): ProjectComponent
     }
 }
--- app/src/main/java/no/nordicsemi/android/ei/repository/ProjectDataRepository.kt
+++ app/src/main/java/no/nordicsemi/android/ei/repository/ProjectDataRepository.kt
@@ -4,10 +4,12 @@ package no.nordicsemi.android.ei.repository
 import no.nordicsemi.android.ei.di.ProjectScope
 import no.nordicsemi.android.ei.model.DevelopmentKeys
 import no.nordicsemi.android.ei.model.Project
+import no.nordicsemi.android.ei.model.SocketToken
 import javax.inject.Inject
 
 @ProjectScope
 class ProjectDataRepository @Inject constructor(
     val project: Project,
-    val developmentKeys: DevelopmentKeys
+    val developmentKeys: DevelopmentKeys,
+    val socketToken: SocketToken
 )
