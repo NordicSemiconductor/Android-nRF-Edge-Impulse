dcff03cb1e4f124f65229a9ddb14798b1bd25c45
roshanrajaratnam <roshan.rajaratnam@nordicsemi.no>
2021-09-06 11:32:40 +0200

    Remove unused state

--- app/src/main/java/no/nordicsemi/android/ei/comms/BuildState.kt
+++ app/src/main/java/no/nordicsemi/android/ei/comms/BuildState.kt
@@ -3,7 +3,6 @@ package no.nordicsemi.android.ei.comms
 sealed class BuildState {
     object Unknown : BuildState()
     object Started : BuildState()
-    object Downloading : BuildState()
     object Finished : BuildState()
     data class Error(val reason: String?) : BuildState()
 }
