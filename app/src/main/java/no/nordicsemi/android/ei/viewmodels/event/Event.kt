ef767d8d67263bf31321f08c4c2b932648a12544
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-06-06 11:30:41 +0200

    Remove unused event

--- app/src/main/java/no/nordicsemi/android/ei/viewmodels/event/Event.kt
+++ app/src/main/java/no/nordicsemi/android/ei/viewmodels/event/Event.kt
@@ -10,8 +10,6 @@ package no.nordicsemi.android.ei.viewmodels.event
 
 sealed class Event {
 
-    data object None: Event()
-
     sealed class Project {
         data class Created(val projectName: String): Event()
         data class Selected(val project: no.nordicsemi.android.ei.model.Project): Event()
